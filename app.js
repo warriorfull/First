0e4edde40a88f68b7a8a93e549a22086d1ff73a5
Todd Eric Whitehead <warriorfullarmorofgod@gmail.com>
2022-07-31 06:05:02 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const Person = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('p', {}, props.occupation) 
+    ]);
+}
+const App = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {class: 'title'}, 'React IS rendered'),
+        React.createElement(Person, {name: 'Yihua', occupation: 'instructor'}, null),
+        React.createElement(Person, {name: 'Andrei', occupation: 'Lead instructor'}, null),
+        React.createElement(Person, {name: 'Yihua', occupation: 'teacher'}, null),
+    ]);
+};
+
+// ReactDOM.render(React.createElement(App), document.getElementById('root'));
+   
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
